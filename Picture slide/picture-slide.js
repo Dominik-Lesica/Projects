3d136ad91283cfbbed4bf080412fa43b26adce38
Dominik <lesicadomi22@gmail.com>
2023-10-04 17:45:08 +0200

    reworked js

--- Picture slide/picture-slide.js
+++ Picture slide/picture-slide.js
@@ -1,23 +1,32 @@
 const backButton = document.querySelector('.back-icon');
 const nextButton = document.querySelector('.next-icon');
 const imageWrapper = document.querySelector('.image-wrapper');
-const indexes = [0, 1, 2, 3, 4];
+const imgs = ['img1.jpg', 'img2.jpg', 'img3.jpg', 'img4.jpg', 'img5.jpg'];
 let imgCount = 1;
 
 function nextImage() {
-  imgCount++;
-  imageWrapper.innerHTML = `<img src="img/img${imgCount}.jpg" class="img">`
-  if(imgCount === 5) {
-    imgCount = 0;
+  if (imgCount === 5) {
+    imgCount = 1;
+    console.log(imgCount);
+  } else {
+    imgCount++;
+    console.log(imgCount);
   }
+  
+  const NextImage = imgs[imgCount-1];
+  imageWrapper.innerHTML = `<img src="img/${NextImage}" class="img">`;
 }
 
 function lastImage() {
-  imgCount--;
-  if(imgCount === 0) {
+  if(imgCount === 1) {
     imgCount = 5;
+    console.log(imgCount)
+  } else {
+    imgCount--;
+    console.log(imgCount)
   }
-  imageWrapper.innerHTML = `<img src="img/img${imgCount}.jpg" class="img">`
+  const lastImage = imgs[imgCount-1]
+  imageWrapper.innerHTML = `<img src="img/${lastImage}" class="img">`
   
 }
 
